114aaa9e3e2f9c999382b784c85d55d53e9514c9
jghsantiago91@gmail.com <jghsantiago91@gmail.com>
2026-08-31 07:33:29 +0000

    bronze: aisles
    Added query for aisles (raw) streaming table

---
+++ Week6 - Instacart/transformations/bronze.sql
@@ -0,0 +1,20 @@
+-- =====================================================
+--           Bronze Layer: Data Ingestion
+-- =====================================================
+
+-- =====================================================
+--               Aisles
+-- =====================================================
+CREATE OR REFRESH STREAMING TABLE week6.bronze.aisles
+TBLPROPERTIES (
+  'delta.feature.timestampNtz' = 'supported',
+  'delta.columnMapping.mode' = 'name')
+AS
+SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/aisles/', 
+  format => 'csv',
+  dataAddress => 'aisles',
+  headerRows => 1,
+  inferColumnTypes => false,
+  `cloudFiles.schemaEvolutionMode` => 'none');
+
+  
