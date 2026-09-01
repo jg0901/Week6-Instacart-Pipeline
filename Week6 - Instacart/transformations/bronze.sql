0f5fb93245fd14aea00e66ec4586c2e037932b4b
Shiena Marie <arico.smn@gmail.com>
2026-09-01 17:37:59 +0800

    Update bronze.sql
    test commit

--- Week6 - Instacart/transformations/bronze.sql
+++ Week6 - Instacart/transformations/bronze.sql
@@ -45,3 +45,22 @@ shiena test
 -- =====================================================
 
 
+--FOR ORDER PRODUCTS PRIOR TESTING PULL
+%sql 
+-- 1. Create the table with strict data types
+CREATE TABLE IF NOT EXISTS week6.bronze.order_products_prior ( 
+    order_id INT, 
+    product_id INT, 
+    add_to_cart_order INT, 
+    reordered INT 
+) USING DELTA;
+
+-- 2. Incrementally load the data (handles both the initial 32M rows and future daily updates)
+COPY INTO week6.bronze.order_products_prior
+FROM '/Volumes/week6/bronze/raw_files/'
+FILES = ('order_products__prior.csv')
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header' = 'true', 'inferSchema' = 'false') 
+COPY_OPTIONS ('mergeSchema' = 'false', 'force' = 'false');
+
+
