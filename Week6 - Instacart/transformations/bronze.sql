c7958dfdd415b8185b59038eeb7683decd1dfc4c
shiena.arico@ftwfoundation.org <shiena.arico@ftwfoundation.org>
2026-09-02 01:06:41 +0000

    feature/bronze-ingestion_shiena1

--- Week6 - Instacart/transformations/bronze.sql
+++ Week6 - Instacart/transformations/bronze.sql
@@ -12,6 +12,7 @@ TBLPROPERTIES (
 AS
 SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/aisles/', 
   format => 'csv',
+  dataAddress => 'aisles',
   headerRows => 1,
   inferColumnTypes => false,
   `cloudFiles.schemaEvolutionMode` => 'none');
@@ -26,65 +27,57 @@ TBLPROPERTIES (
 AS
 SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/departments/', 
   format => 'csv',
+  dataAddress => 'departments',
   headerRows => 1,
   inferColumnTypes => false,
   `cloudFiles.schemaEvolutionMode` => 'none');
 
+--
 
 -- =====================================================
 --               Orders
 -- =====================================================
-CREATE OR REFRESH STREAMING TABLE week6.bronze.orders
-TBLPROPERTIES (
-  'delta.feature.timestampNtz' = 'supported',
-  'delta.columnMapping.mode' = 'name')
-AS
-SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/orders/', 
-  format => 'csv',
-  headerRows => 1,
-  inferColumnTypes => false,
-  `cloudFiles.schemaEvolutionMode` => 'none');
+
 
 
 -- =====================================================
 --               Orders_Products_Prior
 -- =====================================================
+
+---ingestion
 CREATE OR REFRESH STREAMING TABLE week6.bronze.order_products_prior
-TBLPROPERTIES (
-  'delta.feature.timestampNtz' = 'supported',
-  'delta.columnMapping.mode' = 'name')
-AS
-SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/order_products_prior/', 
+AS SELECT 
+  CAST(order_id AS INT),
+  CAST(product_id AS INT),
+  CAST(add_to_cart_order AS INT),
+  CAST(reordered AS INT)
+FROM STREAM read_files(
+  '/Volumes/week6/bronze/raw_files/order_products__prior.csv',
   format => 'csv',
-  headerRows => 1,
-  inferColumnTypes => false,
-`cloudFiles.schemaEvolutionMode` => 'none');
+  header => 'true'
+);
+
+--ingestion automating the daily refresh
+CREATE OR REFRESH STREAMING TABLE week6.bronze.order_products_prior 
+SCHEDULE CRON '0 0 2 * * ?' AT TIME ZONE 'UTC' -- Runs daily at 2:00 AM UTC 
+AS SELECT 
+   CAST(order_id AS INT), 
+   CAST(product_id AS INT), 
+   CAST(add_to_cart_order AS INT), 
+   CAST(reordered AS INT) 
+FROM STREAM read_files( 
+   '/Volumes/week6/bronze/raw_files/order_products__prior.csv', 
+   format => 'csv', 
+   header => 'true' 
+); 
 
 -- =====================================================
 --               Orders_Products_Train
 -- =====================================================
-CREATE OR REFRESH STREAMING TABLE week6.bronze.order_products_train
-TBLPROPERTIES (
-  'delta.feature.timestampNtz' = 'supported',
-  'delta.columnMapping.mode' = 'name')
-AS
-SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/order_products_train/', 
-  format => 'csv',
-  headerRows => 1,
-  inferColumnTypes => false,
-  `cloudFiles.schemaEvolutionMode` => 'none');
+
 
 
 -- =====================================================
 --               Products
 -- =====================================================
-CREATE OR REFRESH STREAMING TABLE week6.bronze.products
-TBLPROPERTIES (
-  'delta.feature.timestampNtz' = 'supported',
-  'delta.columnMapping.mode' = 'name')
-AS
-SELECT * FROM STREAM read_files('/Volumes/week6/bronze/raw_files/products/', 
-  format => 'csv',
-  headerRows => 1,
-  inferColumnTypes => false,
-  `cloudFiles.schemaEvolutionMode` => 'none');
+
